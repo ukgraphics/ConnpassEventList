09443b3565880ad0b8a5ec04b61290536941c8d8
Kuni <kuni.ujiie@grapecity.com>
2018-01-15 16:47:42 +0900

    Updated.

--- ConnpassEventList/ConnpassEventList/MainPage.xaml.cs
+++ ConnpassEventList/ConnpassEventList/MainPage.xaml.cs
@@ -24,13 +24,6 @@ namespace ConnpassEventList
 
         public void OnSelection(object sender, SelectedItemChangedEventArgs e)
         {
-            //if (e.SelectedItem == null)
-            //{
-            //    return; //ItemSelected is called on deselection, which results in SelectedItem being set to null
-            //}
-            //DisplayAlert("Item Selected", e.SelectedItem.ToString(), "Ok");
-            ////((ListView)sender).SelectedItem = null; //uncomment line if you want to disable the visual selection state.
-
             var selecteditem = e.SelectedItem as Event.EventItem;
 
             Navigation.PushAsync(new DetailPage(selecteditem), true);
