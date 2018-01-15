525c51950a41eb1504e8f49c1821874b1c10df7f
Kuni <kuni.ujiie@grapecity.com>
2018-01-15 16:39:07 +0900

    プロジェクト ファイルを追加します。

---
+++ ConnpassEventList/ConnpassEventList/DetailPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ConnpassEventList
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DetailPage : ContentPage
+	{
+		public DetailPage (Event.EventItem eventItem)
+		{
+			InitializeComponent ();
+
+            var htmlSource = new HtmlWebViewSource();
+            htmlSource.Html = eventItem.description;
+            detail.Source = htmlSource;
+
+            //detail.Source = eventItem.event_url;
+
+        }
+	}
+}
---
+++ ConnpassEventList/ConnpassEventList/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace ConnpassEventList
+{
+    public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            var imageSource = ImageSource.FromResource("ConnpassEventList.connpass_logo_1.png", Assembly.GetExecutingAssembly());
+            logo.Source = imageSource;
+        }
+
+        public void GetEvent(object sender, System.EventArgs e)
+        {
+            List<Event.EventItem> result;
+            result = Event.GetEvent(keyword.Text);
+
+            list.ItemsSource = result;
+        }
+
+        public void OnSelection(object sender, SelectedItemChangedEventArgs e)
+        {
+            //if (e.SelectedItem == null)
+            //{
+            //    return; //ItemSelected is called on deselection, which results in SelectedItem being set to null
+            //}
+            //DisplayAlert("Item Selected", e.SelectedItem.ToString(), "Ok");
+            ////((ListView)sender).SelectedItem = null; //uncomment line if you want to disable the visual selection state.
+
+            var selecteditem = e.SelectedItem as Event.EventItem;
+
+            Navigation.PushAsync(new DetailPage(selecteditem), true);
+        }
+    }
+}
