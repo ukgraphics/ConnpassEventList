e41c8894a72b430cfacd7b229275b879d616c172
Kuni <kuni.ujiie@grapecity.com>
2018-01-16 17:07:17 +0900

    Updated.

--- ConnpassEventList/ConnpassEventList/DetailPage.xaml.cs
+++ ConnpassEventList/ConnpassEventList/DetailPage.xaml.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ConnpassEventList
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class DetailPage : ContentPage
 	{
 		public DetailPage (Event.EventItem eventItem)
--- ConnpassEventList/ConnpassEventList/Event.cs
+++ ConnpassEventList/ConnpassEventList/Event.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace ConnpassEventList
 {
